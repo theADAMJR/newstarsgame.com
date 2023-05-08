dfa0d1dc3e2706308b9dd06df6210b896554111f
ADAMJR <adamjf123@gmail.com>
2023-05-08 01:25:10 +0100

    Start

---
+++ public/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('mousemove', function (e) {
+    moveItem(e, document.querySelector('.parallax-foreground'), 20);
+    moveItem(e, document.querySelector('.parallax-midground'), 40);
+    moveItem(e, document.querySelector('.parallax-background'), 80);
+});
+
+function moveItem(e, parallaxItem, str = 1) {
+    const xPos = (window.innerWidth / 2 - e.clientX) / str;
+    const yPos = (window.innerHeight / 2 - e.clientY) / str;
+    parallaxItem.style.transform = `translate(${xPos}px, ${yPos}px)`;
+}
+
+const getRandom = () => Math.floor(Math.random() * 3) + 1;
+document.querySelector('.parallax-background').style.backgroundImage = `'./Purple/Layer 3/Space Background ${getRandom()}.png'`;
+document.querySelector('.parallax-midground').style.backgroundImage = `'./Purple/Layer 2/Space Background ${getRandom()}.png'`;
+document.querySelector('.parallax-foreground').style.backgroundImage = `'./Purple/Layer 1/Space Background ${getRandom()}.png'`;
