6fb29790c418bbaf1deaf092e5d8b09c30850349
theADAMJR <adamjf123@gmail.com>
2023-07-29 18:35:54 +0100

    Updates

---
+++ server/flight-server-css-manifest.js
@@ -0,0 +1,20 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\src\\app\\changelog\\page.tsx": [
+      "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\src\\app\\changelog\\changelog.css"
+    ],
+    "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\src\\app\\layout.tsx": [
+      "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\src\\app\\globals.css",
+      "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\src\\app\\theme.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\src\\app\\changelog\\page": [
+      "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\src\\app\\changelog\\changelog.css",
+      "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\src\\app\\globals.css",
+      "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\src\\app\\theme.css",
+      "C:\\Users\\adamj\\Documents\\Projects\\newstarsgame.com\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
