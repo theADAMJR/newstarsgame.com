a4d95896167c6100f1fa3c2761438dee6ffc06e5
theADAMJR <adamjf123@gmail.com>
2024-04-05 20:49:49 +0100

    20:49

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,14 +5,16 @@ export default function Navbar() {
     return (
         <div className="flex flex-col items-center justify-between" style={{
             position: 'absolute',
+            alignItems: 'center',
+            width: '100%',
             top: '32px',
-            right: '50%'
+            right: '0%'
         }}>
             <nav className='items-center justify-center gap-x-4 text-3xl'>
                 <Link href="/" className='px-5'>Home</Link>
-                <Link href="/new_worlds" className='px-5'>New Worlds</Link>
                 {/* <Link href="/roadmap" className='px-5'>Roadmap</Link> */}
                 <Link href="/changelog" className='px-5'>Changelog</Link>
+                <Link href="/new_worlds" className='px-5'>New Worlds</Link>
             </nav>
         </div>
     );
