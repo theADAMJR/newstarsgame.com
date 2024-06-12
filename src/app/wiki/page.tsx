0e76a6486c38e53ec30c3d6d0361e7f24edcb078
theADAMJR <adamjf123@gmail.com>
2024-06-12 19:07:34 +0100

    19:07

---
+++ src/app/wiki/page.tsx
@@ -0,0 +1,16 @@
+// pages/wiki.js
+import Sidebar from '../../components/sidebar/sidebar';
+
+const WikiPage = () => {
+  return (
+    <div>
+      <Sidebar />
+      <div>
+        {/* Main content goes here */}
+        <h1>Welcome to the Wiki</h1>
+      </div>
+    </div>
+  );
+};
+
+export default WikiPage;
