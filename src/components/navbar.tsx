570ba7f94bf3b851e2bb0b05698d62597718ece1
theADAMJR <adamjf123@gmail.com>
2024-07-16 09:03:28 +0100

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,6 @@ export default function Navbar() {
         }}>
             <nav className='items-center justify-center gap-x-4 text-3xl'>
                 <Link href="/" className='px-5'>Home</Link>
-                {/* <Link href="/roadmap" className='px-5'>Roadmap</Link> */}
                 <Link href="/changelog" className='px-5'>Changelog</Link>
                 <Link href="https://wiki.newstarsgame.com" className='px-5'>Wiki</Link>
                 <a href="https://adamjr.xyz" className='px-5' target="_blank">Community</a>
